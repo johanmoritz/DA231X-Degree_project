1dfb65f1f60130d8b20cdefdf0b4692a7a2ac3c4
Johan Moritz <jmoriz@kth.se>
2022-02-17 16:12:46 +0100

    Add script for importing references from zotero

---
+++ thesis/import_bib.py
@@ -0,0 +1,26 @@
+from bisect import bisect_left
+import os
+import glob
+
+zotero_export_parh = "/mnt/c/Users/johan.moritz/Downloads"
+bibliography_file = "references.bib"
+
+project_folder_path = os.path.dirname(os.path.abspath(__file__))
+
+exported_file = glob.glob(zotero_export_parh + "/*.bib")[0]
+
+with open(exported_file, "r") as e_file:
+    content = e_file.readlines()
+
+# Remove exported file
+os.remove(exported_file)
+
+with open(bibliography_file, "a") as b_file:
+    b_file.write("\n")
+    b_file.writelines(content)
+
+ref_tmp = next(filter(lambda x: "@" in x, content))
+ref = ref_tmp[ref_tmp.index("{")+1:ref_tmp.index(",")]
+title = next(filter(lambda x: "\ttitle =" in x, content)).split("{")[1].split("}")[0]
+
+print("Imported " + title + " with ref " + ref)
